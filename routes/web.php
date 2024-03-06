638d4d5ddafdef76012c2182de1aec446e033020
MarioFlores44 <123104623+MarioFlores44@users.noreply.github.com>
2024-03-06 17:00:55 +0100

    Prova funcionament 1

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('resources/controllers/index.blade.php');
+    return redirect()->route('index')->to('/resources/controllers/index.blade.php');
 })->name('index');
 
 require __DIR__.'/auth.php';
