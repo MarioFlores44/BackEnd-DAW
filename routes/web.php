928229498c8963500b80a8f5ce6bf865b6b0c55b
MarioFlores44 <123104623+MarioFlores44@users.noreply.github.com>
2024-03-06 16:57:22 +0100

    Modificacio web.php 0.1

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('controllers/index.blade.php');
-});
+    return view('resources/controllers/index.blade.php');
+})->name('index');
 
 require __DIR__.'/auth.php';
