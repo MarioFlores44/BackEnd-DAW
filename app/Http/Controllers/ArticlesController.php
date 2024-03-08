991d10eff5c24e6bd1b6eab839f4e3cafaff7534
MarioFlores44 <123104623+MarioFlores44@users.noreply.github.com>
2024-03-08 16:34:50 +0100

    Proba 1.4 articles

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use App\Models\Article;
+use App\Models\Articles;
 
 class ArticlesController extends BaseController
 {
