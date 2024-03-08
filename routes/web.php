9425ab46591c1eda26451776e09ca6edc8fc85e8
MarioFlores44 <123104623+MarioFlores44@users.noreply.github.com>
2024-03-08 16:31:18 +0100

    Proba 1.1 articles

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view("index");
-})->name('index');
+Route::get('/', [ArticlesController::class, 'show'])->name('index');
 
 require __DIR__.'/auth.php';
