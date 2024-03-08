ba8e5118b931a2f5d820576c12a5523153ed5fb7
MarioFlores44 <123104623+MarioFlores44@users.noreply.github.com>
2024-03-08 16:33:48 +0100

    Proba 1.2 articles

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\Article;
 
 class ArticlesController extends BaseController
 {
     function show() {
-        $data = Article::paginate(5);
+        $data = Articles::paginate(5);
         return view('articles', ['articles' => $data]);
     }
 }
