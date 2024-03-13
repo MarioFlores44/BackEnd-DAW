2d5fdd1dac07656ca6890de47364d2bdb3430854
MarioFlores44 <123104623+MarioFlores44@users.noreply.github.com>
2024-03-13 19:18:00 +0100

    Proba 1.1 Delete

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('nouArticle', [ArticlesController::class, 'create'])->name('nouArticl
 
 Route::post('nouArticle', [ArticlesController::class, 'store']);
 
-Route::get('editar')->name('editar');
+Route::get('update')->name('update');
 
 Route::get('delete/{id}', [ArticlesController::class, 'delete'])->name('delete');
 
