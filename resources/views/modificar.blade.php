5dc63c37d8e21353d5fe7bf8a37352354a2b6f00
MarioFlores44 <123104623+MarioFlores44@users.noreply.github.com>
2024-03-15 15:49:22 +0100

    Proba 1.5 Update

--- resources/views/modificar.blade.php
+++ resources/views/modificar.blade.php
@@ -27,7 +27,7 @@
 				<li>
 					<form action="{{ route('update') }}" method="POST">
 						@csrf
-						<input type="hidden" name="id" value="{{ $article->id }}">
+						<input type="text" name="id" value="{{ $article->id }}" disabled>
 						<input type="text" name="contingut" value="{{ $article->article }}">
 						<input type="submit" value="Update">
 					</form>
