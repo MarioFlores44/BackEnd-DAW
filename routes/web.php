54fc0db1923078757115de4817ffb32cb1e6ce6f
MarioFlores44 <123104623+MarioFlores44@users.noreply.github.com>
2024-03-15 16:13:54 +0100

    Proba 1.2 Profile

--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,6 @@ Route::get('profile', [ProfileController::class, 'edit'])->name('profile');
 
 Route::post('profile', [ProfileController::class, 'update'])->name('profile.update');
 
+Route::delete('profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
 require __DIR__.'/auth.php';
